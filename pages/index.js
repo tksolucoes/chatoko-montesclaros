b1ce47461bd4661058af42775bc95a81af3cd266
tksolucoes <tksolucoesdigitais@gmail.com>
2025-07-11 11:41:28 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ color: '#fff', backgroundColor: '#111', minHeight: '100vh', padding: 20 }}>
+      <h1>Chatoko está online!</h1>
+      <p>Projeto básico Next.js com Firebase em construção...</p>
+    </div>
+  );
+}
